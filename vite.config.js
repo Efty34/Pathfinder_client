8ed002bc00c24d694415c1875df78513817eb1dd
Efty34 <efty17.hasan@gmail.com>
2025-04-06 22:51:23 +0600

    config file modification

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/Pathfinder_client",
   resolve: {
     alias: {
       '@': path.resolve('src'),
